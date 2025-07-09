4dbadb14c04554e9ac69f716494ea65ad6a99d12
tibebu28 <ahadu2821@gmail.com>
2025-07-09 04:01:06 -0700

    fix

---
+++ C+/Data Types.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+	int age;
+	age = 19;
+	string name = "Tibebu";
+	cout<<"my name is:"<<name<<endl;
+	cout<<"my age is:"<<age<<endl;
+	return 1;
+}
---
+++ C+/variables.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+	int age;
+	age = 19;
+	cout<<"my age is:"<<age<<endl;
+	return 1;
+}
+
