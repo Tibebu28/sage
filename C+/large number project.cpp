e113ab6f061fee235b59d5f15cb85e81e4442713
tibebu28 <ahadu2821@gmail.com>
2025-07-11 04:12:00 -0700

    large number project

---
+++ C+/large number project.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	int a;
+	int b;
+	
+	cout<<"Enter fn:"<<endl;
+	cin>>a;
+	cout<<"Enter sn"<<endl;
+	cin>>b;
+	if(a>b){
+		cout<<"max"<<a<<endl;
+	}
+	else{
+		cout<<b<<endl;
+	}
+
+     
+	return 0;
+}
