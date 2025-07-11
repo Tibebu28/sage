279e217c3f32473158cfd15d36cac2432bde181b
tibebu28 <ahadu2821@gmail.com>
2025-07-11 04:03:52 -0700

    celsis_to_fahrenheit

--- C+/celsius_to_fahrnhgit.cpp
+++ C+/celsius_to_fahrnhgit.cpp
@@ -5,14 +5,12 @@ using namespace std;
 void celsius_to_fahrnhgit(double cel){
    	double f;
      	
-   	f=(cel*1.8)+32;
-	cout<<f<<endl;
-}
-
+         	f=(cel*1.8)+32;
+         	cout<<f<<endl;
+	}
 
 
 int main(){
 	celsius_to_fahrnhgit(0);
-     
-	return 0;
+	 return 0;
 }
