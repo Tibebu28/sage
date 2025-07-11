b461f83647543d9bf0c49140460ec58f7ce5786c
tibebu28 <ahadu2821@gmail.com>
2025-07-11 03:44:12 -0700

    celsis_to_fahrenheit

---
+++ C+/celsius.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	double Cel;
+	double F;
+	
+	cout<<"Enter c :"<<endl;
+	cin>>Cel;
+	F=Cel*1.8+32;
+	cout<<"C*1.8+32="<<F;
+	
+
+     
+	return 0;
+}
---
+++ C+/celsius_to_fahrnhgit.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+void celsius_to_fahrnhgit(double cel){
+   	double f;
+     	
+   	f=(cel*1.8)+32;
+	cout<<f<<endl;
+}
+
+
+
+int main(){
+	celsius_to_fahrnhgit(0);
+     
+	return 0;
+}
