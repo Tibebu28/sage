6cbda25887781f17e21edeb163ccbfafba885a74
tibebu28 <ahadu2821@gmail.com>
2025-07-14 03:15:23 +0300

    positive, negative and zero

---
+++ C+/Q 1.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+        int i;
+        
+        	cin>>i;
+        
+        if(i>0){
+        
+		cout<<"positive"<<endl;
+        	
+		}
+		
+		else if(i==0){
+			
+			cout<<"zero"<<endl;
+			
+		}
+		
+		else{
+			
+			cout<<"negative";
+		}
+		
+		return 0;
+}
