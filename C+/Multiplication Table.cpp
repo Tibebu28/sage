bbddd313cab27eec99f8de11840a4156cbb672b6
tibebu28 <ahadu2821@gmail.com>
2025-07-21 03:26:01 +0300

    multiplication table

--- C+/Multiplication Table.cpp
+++ C+/Multiplication Table.cpp
@@ -1,44 +1,21 @@
 #include <iostream>
-
+#include <iomanip>
 using namespace std;
 
 int main(){
 	
-	 int a;
-	 int b;
-	 char m;
-	
-	cout<<"        Multiplication table"<<endl<<endl;
-	cout<<"  to add (A)"<<endl;
-	cout<<"  to subtract (S)"<<endl;
-	cout<<"  to multiply (M)"<<endl;
-	cout<<"  to divide (D)"<<endl<<endl<<endl;
-	cout<<"Type here :";
-	cin>>m;
-	
-			if(m=='A'){
-				cout<<"hello";
-			}
-				else if(m==){
-				
-			}
-			
-					else if(){
-				
-			}
-			
-							else if(){
-				
-			}
+	for(int i=1; i<=10;i++){
 		
+		for(int j=1; j<=10; j++){
 			
-			else{
-				cout<<"m";
-			}
+			 cout<<setw(4)<< i*j;
+		}
+		 cout<<endl;
+	}
 	
 	
 	
 	
-     
+	
 	return 0;
 }
