a3b4063fa6e8be7f556943f6bc253dd5b6f02d64
Tibebu28 <ahadu2821@gmail.com>
2025-07-22 02:39:23 -0700

    javascript

---
+++ HTML, CSS, JAVA SCRIPT/myscript.js
@@ -0,0 +1,41 @@
+let x = 3;
+let y =4 ;            //variable
+let sum =x + y;
+let isjavascript = true;
+
+let Grade = ["A","B","C","D","F"] //array
+
+let person = {name: "Abebe", age: 20, gender: "female"}  //objec
+
+
+
+
+
+
+console.log("the sum is :",sum); //for variable
+
+console.log("is it javascript program :",isjavascript) //for javascript
+
+console.log(Grade[2]); //for array
+
+console.log("person namr:", person.name) // for object
+
+               //condtions
+
+let age = 17;
+
+if(age >= 18){
+    console.log("adult");
+}
+else{
+    console.log("teen");
+}  
+
+            //loop
+
+let i = -5;
+
+while(i <= 5){
+    console.log(i);
+    i++
+}
---
+++ HTML, CSS, JAVA SCRIPT/script.js
@@ -0,0 +1,9 @@
+function greet(){
+    alert('hi javascript from script')
+}
+function add(){
+    let x=1;
+    let y=3;
+    y=7;
+    alert(x=y)
+}
