ee74af72d47af5c2f5dd703168aa559beb29be4f
Tibebu28 <ahadu2821@gmail.com>
2025-07-31 04:04:40 -0700

    React

---
+++ react/sage-project/src/App.js
@@ -0,0 +1,12 @@
+ import Button from "./components/Button"
+ import "./App.css"
+ function App(){
+  return <div>
+              <h1>first component</h1>
+                  <Button name="Log in "/>
+                  <br/><br/>
+                  <Button name="Register" />
+         </div>
+ }
+
+  export default App
