8a37b467ab710cf1d86b800c7a6020115191e8c3
tibebu28 <ahadu2821@gmail.com>
2025-09-03 15:28:15 +0300

    authuntication

--- node js/mongoose/index.js
+++ node js/mongoose/index.js
@@ -1,42 +1,90 @@
-const express = require('express');
-const app = express();
-const mongoose = require("mongoose");
+const express = require("express")
+const app = express()
+const mongoose = require('mongoose')
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
+const key = "hellow"
+
 app.use(express.json())
-mongoose.connect('mongodb+srv://Tibebu28:Addis123!@@cluster0.vsclnww.mongodb.net/students?retryWrites=true&w=majority&appName=Cluster0')
-.then(()=> console.log("MongoDB connected"))
-.catch((err)=> console.log(err))
 
-
-const bookSchema = new mongoose.Schema({
-    title: {type: String, required: true},
-    author: {type: String, required: true},
-    page: {type: Number, min:1}
-
+mongoose.connect("mongodb+srv://Tibebu28:Abebe123@cluster0.vsclnww.mongodb.net/electronic?retryWrites=true&w=majority&appName=Cluster0")
+.then(()=>{
+    console.log("mongoDB conncted")
 })
 
-const BookModel = mongoose.model("Book",bookSchema)
-
-app.post("/books/insert", async (req,res)=>{
-    const book = new BookModel(req.body)
-    await book.save()
-    res.status(200).json(book)
-
+const merchantSchema = new mongoose.Schema({
+    dealer: {type: String, required: true},
+    dealerNumber: {type: Number, unique: true},
+    password: String,
+    role: {type: String, enum:["merchant", "admain"], default: "merchant"}
 })
-app.get("/book", async(req,res)=>{
-    const books = await BookModel.find()
-    res.status(200).json(books)
+
+const Merchant = mongoose.model("merchant",merchantSchema)
+
+app.post("/register",async(req,res)=>{
+    const {dealer, dealerNumber, password} = req.body
+    const hashedPassword = await bcrypt.hash(password,10)
+    const merchant = new Merchant({dealer, dealerNumber, password:hashedPassword})
+    await merchant.save()
+
+    res.status(200).json(merchant)
+})
+
+app.post("/login",async(req,res)=>{
+    const {dealerNumber,password} = req.body
+    const merchant = await Merchant.findOne({dealerNumber})
+    if(!merchant) return res.status(404).json({message:"user not found"})
+
+    const isMatch = await bcrypt.compare(password, merchant.password)
+    if(!isMatch) return res.json({message:"invalid password"})
     
-})
-app.get("/books/:id",(req,res)=>{
+
+    const token = jwt.sign({id:merchant._id,dealerNumber: merchant.dealerNumber, role:merchant.role},key)
     
+    return res.json({dealerNumber: merchant.dealerNumber,token:token})
 })
-app.put("/books/edit/:id",(req,res)=>{
-    
-})
-app.delete("/books/remove/:id",(req,res)=>{
-    
-})
-app.listen(3003,()=>{
-    console.log("srver started at port 3003")
+
+function open(req,res,next) {
+    const header  = req.headers["authorization"]
+    const token = header && header.split(" ")[1]
+    if(!token) return res.sendStatus(401)
+
+        jwt.verify(token,key,(err,decoded)=>{
+            if(err) return res.sendStatus(403)
+                console.log("decoded"+decoded)
+                req.merchant = decoded
+            next()
+        })
 }
-)
+
+function role(requiredrole){
+    return (req,res,next)=>{
+        if(req.merchant.role !== requiredrole){
+                return res.status(401).json({message:"you don't have permisson"})
+        }
+        next()    
+
+    }
+}
+
+
+app.get("/profile", open, async(req,res)=>{
+    const merchant = await Merchant.findOne({dealerNumber: req.merchant.dealerNumber})
+    if(!merchant){
+        return res.status(404).json({message:"merchat not found"})
+    }
+    res.status(200).json({
+        message:"hi",
+        dealerNumber: merchant.dealerNumber,
+        dealer: merchant.dealer
+    })
+    
+})
+app.get("/admain",open,role("admain"),(req,res)=>{
+    res.json({message:"welcome admin"})
+    
+})
+
+app.listen(3002,()=>{
+    console.log("server started")
+})
--- node js/server/index.js
+++ node js/server/index.js
@@ -14,9 +14,34 @@ const bookSchema = new mongoose.schema({
  
  const book = mongoose.model("book",bookSchema)
 
- app.post("/register",(req,res)=>{
-    const {useName,password} = req.body
-    const hasedPassword = await bcrypt.hash(password,10)
-    const isMatch = await bcrypt.compare(password, book.password)
- })
+app.post('/register',(req,res)=>{
+   const {userName,password} = req.body
+   const hashedPassword = await bcrypt.hash(password,10)
+   const user = new User({userName,password:hashedPassword})
+   await user.save()
+})
 
+app.post('/login',(req,res)=>{
+   const {userName,password} = req.body
+
+   const user = await User.find(u => user === u.user)
+   if(user) return res.sendStatus(409)
+
+
+   const isMatch = await bcrypt.compare(password,user.password)
+
+  const token = jwt.sign({userName: }, key)
+})
+
+function midlle(req,res,next){
+   const header = req.header['authoration']
+   const token = header && header.split(" ")[1]
+   if (!token) return res.sendSend(401)
+
+
+   jwt.verify(token,key,(err,u)=>{
+      if(err) return res.sendStatus
+         req.user = user
+      next()
+   })
+}
