c0d534e79a5ef19d58eb8b6544891bb4748baecb
Tibebu28 <ahadu2821@gmail.com>
2025-09-03 14:04:04 +0300

    auth

--- node js/mongoose/index.js
+++ node js/mongoose/index.js
@@ -1,42 +1,84 @@
-const express = require('express');
-const app = express();
-const mongoose = require("mongoose");
+const express = require("express")
+const app = express()
+const mongoose = require('mongoose')
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
+const key = "hellow"
+
 app.use(express.json())
-mongoose.connect('mongodb+srv://Tibebu28:Addis123!@@cluster0.vsclnww.mongodb.net/students?retryWrites=true&w=majority&appName=Cluster0')
-.then(()=> console.log("MongoDB connected"))
-.catch((err)=> console.log(err))
 
-
-const bookSchema = new mongoose.Schema({
-    title: {type: String, required: true},
-    author: {type: String, required: true},
-    page: {type: Number, min:1}
-
+mongoose.connect("mongodb+srv://Tibebu28:Abebe123@cluster0.vsclnww.mongodb.net/electronic?retryWrites=true&w=majority&appName=Cluster0")
+.then(()=>{
+    console.log("mongoDB conncted")
 })
 
-const BookModel = mongoose.model("Book",bookSchema)
-
-app.post("/books/insert", async (req,res)=>{
-    const book = new BookModel(req.body)
-    await book.save()
-    res.status(200).json(book)
-
+const mobileSchema = new mongoose.Schema({
+    merchant: {type: String, required: true},
+    merchantNumber: {type: Number, unique: true},
+    password: String
 })
-app.get("/book", async(req,res)=>{
-    const books = await BookModel.find()
-    res.status(200).json(books)
+
+const Mobile = mongoose.model("mobile",mobileSchema)
+
+app.post("/register",async(req,res)=>{
+    const {merchant, merchantNumber, password} = req.body
+    const hashedPassword = await bcrypt.hash(password,10)
+    const mobile = new Mobile({merchant, merchantNumber, password:hashedPassword})
+    await mobile.save()
+
+    res.status(200).json(mobile)
+})
+
+app.post("/login",async(req,res)=>{
+    const {merchantNumber,password} = req.body
+    const mobile = await Mobile.findOne({merchantNumber})
+    if(!mobile) return res.status(404).json({message:"user not found"})
+
+    const isMatch = await bcrypt.compare(password, mobile.password)
+    if(!isMatch) return res.json({message:"invalid password"})
     
-})
-app.get("/books/:id",(req,res)=>{
+
+    const token = jwt.sign({merchantNumber: mobile.merchantNumber},key)
     
+    return res.json({merchantNumber: mobile.merchantNumber,token:token})
 })
-app.put("/books/edit/:id",(req,res)=>{
-    
-})
-app.delete("/books/remove/:id",(req,res)=>{
-    
-})
-app.listen(3003,()=>{
-    console.log("srver started at port 3003")
+
+function open(req,res,next) {
+    const header  = req.header["authorization"]
+    const token = header && header.split(" ")[1]
+    if(!token) return res.sendStatus(401)
+
+        jwt.verify(token,key,(err,decoded)=>{
+            if(err) return res.sendStatus(403)
+                req.mobile = decoded
+            next()
+        })
 }
-)
+
+function role(requiredrole){
+    return (req,res,next)=>{
+        if(req.mobile.role !== requiredrole){
+                return res.status(401).json({message:"you don't have permisson"})
+        }
+        next()    
+
+    }
+}
+
+
+app.get("/merchat/profile",open,async(req,res)=>{
+    const merchant = await Mobile.findOne({merchantNumber: req.mobile.merchantNumber})
+    if(!merchant){
+        return res.status(401).json({message:"merchat not found"})
+    }
+    res.status(200).json("Hi merchat")
+    
+})
+app.get("/admain",open,role("admain"),(req,res)=>{
+    res.json({message:"welcome admin"})
+    
+})
+
+app.listen(3002,()=>{
+    console.log("server started")
+})
